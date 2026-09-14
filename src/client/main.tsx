5d4d4a2ae061b01ef5115ad94c246c553e7b81e6
K H Arjun <kharjun48@gmail.com>
2026-09-14 11:36:24 +0530

    update archive header text to STILL BUILDING / STILL LEARNING

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -731,7 +731,7 @@ function App() {
 
       <section id="archive" className="archive-section section-pad">
         <Command>ls /archive</Command>
-        <div className="section-heading archive-heading"><ScrollLetterReveal text="A HISTORY OF MAKING" breaks={[10]}/><span>FAILED OPENLY<br/>LEARNED QUIETLY</span></div>
+        <div className="section-heading archive-heading"><ScrollLetterReveal text="A HISTORY OF MAKING" breaks={[10]}/><span>STILL BUILDING<br/>STILL LEARNING</span></div>
         <div className="archive-list">
           {archiveItems.map((item, i) => {
             const open = openArchive === i;
